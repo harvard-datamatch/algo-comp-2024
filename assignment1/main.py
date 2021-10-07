7f7a0e2aa2b31a107d3fc06684ffc471a5128e67
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-10-07 15:11:23 -0400

    Add test data

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,16 +1,43 @@
 #!usr/bin/env python3
+import json
+import sys
+import os
+
+INPUT_FILE = 'testdata.json' # Constant variables are usually in ALL CAPS
+
 class User:
-    def __init__(self, name, grad_year=None, responses=None):
+    def __init__(self, name, gender, preferences, grad_year, responses):
         self.name = name
+        self.gender = gender
+        self.preferences = preferences
         self.grad_year = grad_year
         self.responses = responses
 
 
 # Takes in two user objects and outputs a float denoting compatibility
 def compute_score(user1, user2):
-    # Your code here
+    # YOUR CODE HERE
     return 0
 
 
 if __name__ == '__main__':
-    print('Hello World!')
+    # Make sure input file is valid
+    if not os.path.exists(INPUT_FILE):
+        print('Input file not found')
+        sys.exit(0)
+
+    users = []
+    with open(INPUT_FILE) as json_file:
+        data = json.load(json_file)
+        for user_obj in data['users']:
+            new_user = User(user_obj['name'], user_obj['gender'],
+                            user_obj['preferences'], user_obj['gradYear'],
+                            user_obj['responses'])
+            users.append(new_user)
+
+    for i in range(len(users)-1):
+        for j in range(i+1, len(users)):
+            user1 = users[i]
+            user2 = users[j]
+            score = compute_score(user1, user2)
+            print('Compatibility between {} and {}: {}'.format(user1.name, user2.name, score))
